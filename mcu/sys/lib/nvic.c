7a78ff8474cc927b293d99c78f70d00815e4e0ef
somebyte <somebyte@gmail.com>
2021-05-27 19:15:46 +0300

    Added function NVIC_IRQn_disable

--- mcu/sys/lib/nvic.c
+++ mcu/sys/lib/nvic.c
@@ -17,3 +17,13 @@ NVIC_IRQn_init (uint8_t IRQN, uint8_t PR)
   S32_NVIC->ICPR[ div ] |= 1 << mod;  /* IRQn: clr any pending IRQ */
   S32_NVIC->ISER[ div ] |= 1 << mod;  /* IRQn: enable IRQ          */
 }
+
+void
+NVIC_IRQn_disable (uint8_t IRQN)
+{
+  uint8_t div = IRQN / 32;
+  uint8_t mod = IRQN % 32;
+  S32_NVIC->IP[ IRQN ]   = 0x00000000;             /* IRQn: priority of 0-15 */
+  S32_NVIC->ICPR[ div ] &= ~(uint32_t)(1 << mod);  /* IRQn: clr any pending IRQ */
+  S32_NVIC->ISER[ div ] &= ~(uint32_t)(1 << mod);  /* IRQn: enable IRQ          */
+}
--- mcu/sys/nvic.h
+++ mcu/sys/nvic.h
@@ -10,6 +10,7 @@
 
 #include <stdint.h>
 
-void NVIC_IRQn_init (uint8_t IRQn, uint8_t priority);
+void NVIC_IRQn_init    (uint8_t IRQn, uint8_t priority);
+void NVIC_IRQn_disable (uint8_t IRQn);
 
 #endif /* NVIC_NVIC_H_ */
